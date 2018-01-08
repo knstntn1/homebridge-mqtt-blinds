a75c751f245199d12dcf1437f88e2592256292ed
Tim <timfaltin@gmx.de>
2018-01-08 18:56:29 +0100

    initial upload

---
+++ index.js
@@ -0,0 +1,154 @@
+var mqtt = require("mqtt");
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+
+    homebridge.registerAccessory("homebridge-mqtt-blinds", "BlindsMQTT", BlindsMQTTAccessory);
+}
+
+function BlindsMQTTAccessory(log, config) {
+    // global vars
+    this.log = log;
+
+    // configuration vars
+    this.name = config["name"];
+    this.upURL = config["up_url"];
+    this.downURL = config["down_url"];
+    this.stopURL = config["stop_url"];
+    this.stopAtBoundaries = config["trigger_stop_at_boundaries"];
+    this.httpMethod = config["http_method"] || "POST";
+    this.motionTime = config["motion_time"];
+
+    // state vars
+    this.interval = null;
+    this.timeout = null;
+    this.lastPosition = 0; // last known position of the blinds, down by default
+    this.currentPositionState = 2; // stopped by default
+    this.currentTargetPosition = 0; // down by default
+
+    // register the service and provide the functions
+    this.service = new Service.WindowCovering(this.name);
+
+    // the current position (0-100%)
+    // https://github.com/KhaosT/HAP-NodeJS/blob/master/lib/gen/HomeKitTypes.js#L493
+    this.service
+        .getCharacteristic(Characteristic.CurrentPosition)
+        .on('get', this.getCurrentPosition.bind(this));
+
+    // the position state
+    // 0 = DECREASING; 1 = INCREASING; 2 = STOPPED;
+    // https://github.com/KhaosT/HAP-NodeJS/blob/master/lib/gen/HomeKitTypes.js#L1138
+    this.service
+        .getCharacteristic(Characteristic.PositionState)
+        .on('get', this.getPositionState.bind(this));
+
+    // the target position (0-100%)
+    // https://github.com/KhaosT/HAP-NodeJS/blob/master/lib/gen/HomeKitTypes.js#L1564
+    this.service
+        .getCharacteristic(Characteristic.TargetPosition)
+        .on('get', this.getTargetPosition.bind(this))
+        .on('set', this.setTargetPosition.bind(this));
+}
+
+BlindsMQTTAccessory.prototype.getCurrentPosition = function(callback) {
+    this.log("Requested CurrentPosition: %s", this.lastPosition);
+    callback(null, this.lastPosition);
+}
+
+BlindsMQTTAccessory.prototype.getPositionState = function(callback) {
+    this.log("Requested PositionState: %s", this.currentPositionState);
+    callback(null, this.currentPositionState);
+}
+
+BlindsMQTTAccessory.prototype.getTargetPosition = function(callback) {
+    this.log("Requested TargetPosition: %s", this.currentTargetPosition);
+    callback(null, this.currentTargetPosition);
+}
+
+BlindsMQTTAccessory.prototype.setTargetPosition = function(pos, callback) {
+    this.log("Set TargetPosition: %s", pos);
+    this.currentTargetPosition = pos;
+    if (this.currentTargetPosition == this.lastPosition) {
+        if (this.interval != null) clearInterval(this.interval);
+        if (this.timeout != null) clearTimeout(this.timeout);
+        this.httpRequest(this.stopURL, this.httpMethod, function() {
+            this.log("Already here");
+        }.bind(this));
+        callback(null);
+        return;
+    }
+    const moveUp = (this.currentTargetPosition >= this.lastPosition);
+    this.log((moveUp ? "Moving up" : "Moving down"));
+
+    this.service
+        .setCharacteristic(Characteristic.PositionState, (moveUp ? 1 : 0));
+
+    this.httpRequest((moveUp ? this.upURL : this.downURL), this.httpMethod, function() {
+        this.log(
+            "Success moving %s",
+            (moveUp ? "up (to " + pos + ")" : "down (to " + pos + ")")
+        );
+        this.service
+            .setCharacteristic(Characteristic.CurrentPosition, pos);
+        this.service
+            .setCharacteristic(Characteristic.PositionState, 2);
+    }.bind(this));
+
+    var localThis = this;
+    if (this.interval != null) clearInterval(this.interval);
+    if (this.timeout != null) clearTimeout(this.timeout);
+    this.interval = setInterval(function(){
+        localThis.lastPosition += (moveUp ? 1 : -1);
+        if (localThis.lastPosition == localThis.currentTargetPosition) {
+            if (localThis.currentTargetPosition != 0 && localThis.currentTargetPosition != 100) {
+                localThis.httpRequest(localThis.stopURL, localThis.httpMethod, function() {
+                    localThis.log(
+                        "Success stop moving %s",
+                        (moveUp ? "up (to " + pos + ")" : "down (to " + pos + ")")
+                    );
+                    localThis.service
+                        .setCharacteristic(Characteristic.CurrentPosition, pos);
+                    localThis.service
+                        .setCharacteristic(Characteristic.PositionState, 2);
+                    localThis.lastPosition = pos;
+                }.bind(localThis));
+            }
+            clearInterval(localThis.interval);
+        }
+    }, parseInt(this.motionTime) / 100);
+    if (this.stopAtBoundaries && (this.currentTargetPosition == 0 || this.currentTargetPosition == 100)) {
+        this.timeout = setTimeout(function() {
+            localThis.httpRequest(localThis.stopURL, localThis.httpMethod, function() {
+                localThis.log(
+                    "Success stop adjusting moving %s",
+                    (moveUp ? "up (to " + pos + ")" : "down (to " + pos + ")")
+                );
+            }.bind(localThis));
+        }, parseInt(this.motionTime));
+    }
+    callback(null);
+}
+
+BlindsMQTTAccessory.prototype.httpRequest = function(url, method, callback) {
+    request({
+        method: method,
+        url: url,
+    }, function(err, response, body) {
+        if (!err && response && response.statusCode == 200) {
+            callback(null);
+        } else {
+            this.log(
+                "Error getting state (status code %s): %s",
+                (response ? response.statusCode : "not defined"),
+                err
+            );
+            callback(err);
+        }
+    }.bind(this));
+}
+
+BlindsMQTTAccessory.prototype.getServices = function() {
+    return [this.service];
+}
